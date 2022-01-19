873b69122c9199fa0a9257be9b25473df55eb5a1
Fattelo da Solo <98049634+Fattelodasolo@users.noreply.github.com>
2022-01-19 19:18:07 +0100

    Add files via upload

---
+++ stampa messaggio.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+	
+	std::cout << "Hello World! " <<std::endl;
+	
+	std::cout <<"Hello \nWorld! " <<std::endl;
+	
+	std::cout << "Hello \tWorld!";
+	
+	return 0;
+}
