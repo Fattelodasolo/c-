9a8c0fe6117f34c15ee91168ba0ed868dbaac872
Fattelo da Solo <98049634+Fattelodasolo@users.noreply.github.com>
2022-05-11 09:04:04 +0200

    Add files via upload

---
+++ prodotto tra numeri 1 e 9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int n = 1;
+	
+	for (int i = 1; i <= 9; i++){
+		n = n * i;
+	}
+	
+
+	cout <<"Il prodotto dei numeri tra 1 e 9 corrisponde a: " << n << endl;
+	
+return 0;
+}
+
---
+++ somma numeri tra 0 e 9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int n = 0;
+	
+	for (int i = 0; i <= 9; i++){
+		n = n + i;
+	}
+	
+
+	cout <<"La somma dei numeri tra 0 e 9 corrisponde a: " << n << endl;
+	
+return 0;
+}
+
